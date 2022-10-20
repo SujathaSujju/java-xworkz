206642fec00a6183a2298e0cbeefe091d0264e3e
SujathaSujju <sujatha.xworkz@gmail.com>
2022-10-21 00:23:45 +0530

    programs

---
+++ Main.java
@@ -0,0 +1,18 @@
+public class Main{  
+ public static void main(String []args){  
+
+        int i,j,lines=5; 
+ 
+   for(i=1;i>=lines;i++){     
+                                                  // this loop is used to print the lines  
+      for(j=lines;j>=1;--j){
+                                                  // this loop is used to print numbers in a line  
+         
+           if(j>=lines)  
+           System.out.print(i++);  
+           else  
+            System.out.print("*");  
+   }  
+   System.out.println(" ");  
+   }  
+    }}   
